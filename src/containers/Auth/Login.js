f7625cbb41780594373293050c39ee0c61362ccc
Nguyen Minh Duc <nguyenminhduc06072002@gmail.com>
2024-11-09 17:39:19 +0700

    Complete basic CRUD functions for User model

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -79,7 +79,7 @@ class Login extends Component {
                                 onChange={(event) => {this.handleOnChangeInput(event)}}
                                 />
                                 <span onClick={() => {this.handleShowHidePassword()}}>
-                                    <i class={this.state.isShowPassword ? "far fa-eye" : "far fa-eye-slash"}></i>
+                                    <i className={this.state.isShowPassword ? "far fa-eye" : "far fa-eye-slash"}></i>
                                 </span>
                             </div>
                         </div>
